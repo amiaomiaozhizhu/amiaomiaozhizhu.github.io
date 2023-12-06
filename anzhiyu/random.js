165cd493e8bcd6cbf0e91b15e5352f3deabd10da
ammzz <1036623186@qq.com>
2023-12-06 10:12:09 +0800

    Site updated: 2023-12-06 10:12:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/leetcode/LC 131 分割回文串/","2023/12/leetcode/LC 784 字母大小写全排列/","2023/12/leetcode/LC 17 电话号码的数字组合/","2023/12/leetcode/LC 78 子集/"];function toRandomPost(){
+var posts=["2023/12/leetcode/LC 131 分割回文串/","2023/12/leetcode/LC 17 电话号码的数字组合/","2023/12/leetcode/LC 78 子集/","2023/12/leetcode/LC 784 字母大小写全排列/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
